1a7edc54315ce52729326941751d2f3d380d400b
James Theo <jamestheo@Jamess-MacBook-Pro.local>
2017-12-27 13:46:53 +1100

    writing really shit code. not tested. probably doesnt work

--- js/main.js
+++ js/main.js
@@ -7,5 +7,19 @@ document.addEventListener('DOMContentLoaded', function(){
     Action:[],
     World:[]};
 
-  
+  function getAppendElement(element, behaviour, attribute){
+    var el = document.querySelectorAll(element);
+    switch (behaviour) {
+      case 'html':
+      el.innerHTML
+
+        break;
+      case 'attr':
+      el.getAttribute(attribute)
+
+        break;
+      default:
+
+    }
+  }
 }, false);
