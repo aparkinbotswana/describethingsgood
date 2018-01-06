e0e66f3342e1badad8b0c5fb40f4d3e2457cf0fc
James Theo <jamestheo@Jamess-MacBook-Pro.local>
2018-01-06 19:59:03 +1100

    structured html differently. did some js stuff.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
   const topic = [' Object', 'Nature', 'Random', 'Person', 'Action', 'World'];
-  const card = {Objects:[],
-    Nature:[],
-    Random:[],
-    Person:[],
-    Action:[],
-    World:[]};
+  const card = {Objects:['object1', 'object2', 'object3', 'object4'],
+    Nature:['nature1', 'nature2', 'nature3', 'nature4'],
+    Random:['random1', 'random2', 'random3', 'random4'],
+    Person:['person1', 'person2', 'person3', 'person4'],
+    Action:['action1', 'action2', 'action3', 'action4'],
+    World:['world1', 'world2', 'world3', 'world4']};
 
 
   function changeAttr(el, attr, attrProperty){
@@ -14,6 +14,14 @@ document.addEventListener('DOMContentLoaded', function(){
   }
 
 
-  changeAttr('#draw', 'style', 'color:red;')
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * (max - 0 + 1)) + 0;
+  }
 
+  console.log(Object.entries(card));
+
+
+  function drawCard(){
+
+  }
 }, false);
