ea7515d5eda3d2c95ddee068afa8004a58a06740
James Theo <jamestheo@Jamess-MacBook-Pro.local>
2018-01-22 14:04:03 +1100

    finally got the node module working. Fuck im stupid

--- js/main.js
+++ js/main.js
@@ -56,7 +56,38 @@ document.addEventListener('DOMContentLoaded', function(){
     }
   } // takes drawn card out of card array so that it is not drawn again
 
+  // function animationPlay() {
+  //   document.querySelector('.animation-target').setAttribute('style', 'animation-play-state:running;');
+  // }
 
   document.getElementById('draw').addEventListener('click', drawCard);
-  // document.getElementById('draw').addEventListener('click', topicSelect);
+  // document.getElementById('draw').addEventListener('click', animationPlay);
+
+  function animationPlay() {
+    var bounce = new Bounce();
+    bounce
+    .translate({
+      from: { x: -300, y: 0 },
+      to: { x: 0, y: 0 },
+      duration: 600,
+      stiffness: 4
+    })
+    .scale({
+      from: { x: 1, y: 1 },
+      to: { x: 0.1, y: 2.3 },
+      easing: "sway",
+      duration: 800,
+      delay: 65,
+      stiffness: 2
+    })
+    .scale({
+      from: { x: 1, y: 1},
+      to: { x: 5, y: 1 },
+      easing: "sway",
+      duration: 300,
+      delay: 30,
+    })
+    .applyTo(document.querySelectorAll(".animation-target"));
+  }
+  animationPlay();
 }, false);
