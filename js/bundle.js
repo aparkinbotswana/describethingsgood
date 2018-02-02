f92b7b2180b93c6092e7ac74f491c546640cfca4
James Theo <jamestheo@Jamess-MacBook-Pro.local>
2018-02-03 02:21:36 +1100

    node module fixed. Adrian is awesome

--- js/bundle.js
+++ js/bundle.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var bounceJS = require('bounce');
+var Bounce = require('bounce.js');
 document.addEventListener('DOMContentLoaded', function(){
   const topic = [' Object', 'Nature', 'Random', 'Person', 'Action', 'World'];
   var card = {Objects:['object0', 'object1', 'object2', 'object3', 'object4'],
@@ -86,7 +86,7 @@ document.addEventListener('DOMContentLoaded', function(){
   document.getElementById('draw').addEventListener('click', animationPlay);
 }, false);
 
-},{"bounce":2}],2:[function(require,module,exports){
+},{"bounce.js":2}],2:[function(require,module,exports){
 (function (global){
 /**
  * Bounce.js 0.8.2
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var bounceJS = require('bounce');
+var Bounce = require('bounce.js');
 document.addEventListener('DOMContentLoaded', function(){
   const topic = [' Object', 'Nature', 'Random', 'Person', 'Action', 'World'];
   var card = {Objects:['object0', 'object1', 'object2', 'object3', 'object4'],
